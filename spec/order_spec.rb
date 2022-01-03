58cbefe82042c0294433ac6a748871bcd2493a38
Edeman George <71378676+eds-101@users.noreply.github.com>
2022-01-03 15:56:38 +0000

    test for viewing menu loaded w dishes

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,9 +1,15 @@
 require 'order'
 
 describe Order do
-    it 'displays a list of dishes and prices' do
+    it 'relies on a list of dishes' do
         test_order = Order.new
         expect(test_order.view_menu).to eq("There's no items on this menu")
+    end
+
+    it 'can load a menu of dish items' do
+        test_order_2 = Order.new
+        test_order_2.load_menu(menu)
+        expect(test_order.view_menu[0]).to include "Calamari"
     end
     
 end
